53b11c7eada78685addf68cd66818bfc8ccf6544
Desmond Afari <afaridesmond@gmail.com>
2022-09-04 15:02:59 +0000

    styling done

--- core/forms.py
+++ core/forms.py
@@ -9,8 +9,8 @@ class RecipientForm(forms.ModelForm):
 
 
 class EmailForm(forms.Form):
-    sender = forms.EmailField(widget=forms.TextInput(attrs={'value': 'afaridesmond@gmail.com', 'type': 'email'}))
+    sender = forms.EmailField(widget=forms.TextInput(attrs={'value': 'afaridesmond@gmail.com', 'type': 'email', 'placeholder': 'From'}))
     subject = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'placeholder': 'Subject'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Type here...'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Type email here...'}))
 
 
